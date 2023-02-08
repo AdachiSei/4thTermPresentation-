8025898f906e898e9c741ac630f46e154fae63c9
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2023-02-08 11:45:11 +0900

    シーンローダー関連を作った

---
+++ Assets/Scripts/Manager/SceneLoader.cs
@@ -0,0 +1,101 @@
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// スクリプト
+/// </summary>
+public class SceneLoader : MonoBehaviour
+{
+    #region Public Property
+
+    public string CurrentScene => SceneManager.GetActiveScene().name;
+
+    #endregion
+
+    #region Inspector Member
+
+    [SerializeField]
+    [Header("ローディング時に回転する絵")]
+    Image _loadingImage;
+
+    [SerializeField]
+    [Header("ローディング時に表示するパネル")]
+    Image _loadingPanel;
+
+    [SerializeField]
+    [Header("フェードするまでの時間")]
+    private float _fadeTime = 1f;
+
+    [SerializeField]
+    [Header("回転する絵の回転速度")]
+    private float _loadingImageSpeed = 1f;
+
+    #endregion
+
+    #region Private Member
+
+    private Vector3 _rotDir = new Vector3(0f, 0f, -360);
+    private const int LOOP_VALUE = -1;
+
+    #endregion
+
+    #region Constants
+
+    public const float MAX_ALPFA_VALUE = 1f;
+
+    #endregion
+
+    #region Unity Method
+
+    private void Awake()
+    {
+        FadeIn();
+        _loadingImage?.gameObject.SetActive(false);
+    }
+
+    #endregion
+
+    #region Public Method
+
+    async public void LoadScene(string name)
+    {
+        await FadeOut();
+
+        var tween = 
+            _loadingImage?
+                .transform
+                .DORotate(_rotDir, _loadingImageSpeed, 
+                            RotateMode.FastBeyond360)
+                .SetEase(Ease.Linear)
+                .SetLoops(LOOP_VALUE);
+
+        await SceneManager.LoadSceneAsync(name);
+
+        tween.Kill();
+    }
+
+    #endregion
+
+
+    #region Private Methods
+
+    private void FadeIn()
+    {
+        _loadingPanel?.DOFade(0f, _fadeTime);
+    }
+
+    async private UniTask FadeOut()
+    {
+        _loadingPanel.DOFade(MAX_ALPFA_VALUE, _fadeTime);
+
+        await UniTask.Delay(TimeSpan.FromSeconds(_fadeTime));
+    }
+
+    #endregion
+}
