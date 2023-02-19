e1246d3756ee007c62547988b061d596de7f7b21
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2023-02-19 20:18:54 +0900

    モデルをインポートしたときに自動的にヒューマンになるようにした

---
+++ Assets/Scripts/Editor/AssetPostprocessor/AnimationProcessor.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class AnimationProcessor : AssetPostprocessor
+{
+    private void OnPreprocessAnimation()
+    {
+        var animatioImporter = assetImporter as ModelImporter;
+
+        var clips = animatioImporter.clipAnimations;
+
+        if (clips.Length == 0) clips = animatioImporter.defaultClipAnimations;
+
+        foreach (var clip in clips)
+        {
+            clip.loopTime = true;
+        }
+
+        animatioImporter.clipAnimations = clips;
+    }
+}
