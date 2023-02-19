038332f8395e9ed3c9f307bee1a4901fb6ed6556
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2023-02-19 20:44:41 +0900

    初回のみに修正

--- Assets/Scripts/Editor/AssetPostprocessor/AnimationProcessor.cs
+++ Assets/Scripts/Editor/AssetPostprocessor/AnimationProcessor.cs
@@ -8,6 +8,9 @@ public class AnimationProcessor : AssetPostprocessor
     {
         var animatioImporter = assetImporter as ModelImporter;
 
+        //初回インポートのみに制限
+        if (!animatioImporter.importSettingsMissing) return;
+
         var clips = animatioImporter.clipAnimations;
 
         if (clips.Length == 0) clips = animatioImporter.defaultClipAnimations;
